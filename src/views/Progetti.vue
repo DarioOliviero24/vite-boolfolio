432a61e448106b7cc1ac482721818e564c9192a8
DarioOliviero24 <dario_oliviero@yahoo.com>
2024-11-12 14:52:49 +0100

    Aggiunte le 3 page vue

---
+++ src/views/Progetti.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  
+  <router-wiev></router-wiev>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+
+</style>
